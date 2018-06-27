81098916fb8c1b1361bf3af7a62647bc99dbabab
djillucid <djillucid@gmail.com>
2018-06-27 12:07:01 -0700

    Stage SimpleFactory Project

--- SimpleFactory/Program.cs
+++ SimpleFactory/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FactoryPattern
+namespace SimpleFactory
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
-
         }
     }
 }
